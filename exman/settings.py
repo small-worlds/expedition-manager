d04c29e8ba3caef2a22a74d333a77447d949ce0d
Lane Farrow <laneatomic@gmail.com>
2016-10-23 23:28:56 -0500

    fix static root

--- exman/settings.py
+++ exman/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
+STATIC_ROOT = os.path.dirname(__file__)
 
 PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.Argon2PasswordHasher',

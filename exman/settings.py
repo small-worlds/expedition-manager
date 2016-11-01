db3778c0a421d703c5b4b30fa33c9af695b1cd6a
Lane Farrow <laneatomic@gmail.com>
2016-10-31 22:15:27 -0500

    Override email templates

--- exman/settings.py
+++ exman/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'exman.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(PROJECT_ROOT, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

8f6ab5ad9f59f737f762e4101af516e90ef8f936
Lane Farrow <laneatomic@gmail.com>
2016-10-31 22:16:59 -0500

    fix project root

--- exman/settings.py
+++ exman/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'exman.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(PROJECT_ROOT, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

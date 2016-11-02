19df52e089bd9665bd4c5560829bae4f68760091
Lane Farrow <laneatomic@gmail.com>
2016-11-02 18:38:08 -0500

    Force that godforsaken email field to be required

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from accounts.models import Profile
+from djoser import settings
 
 
 class ProfileSerializer(serializers.ModelSerializer):
@@ -14,3 +15,30 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'id', 'date_joined')
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(required=True)
+
+    password = serializers.CharField(style={'input_type': 'password'},
+                                     write_only=True,
+                                     validators=settings.get('PASSWORD_VALIDATORS'))
+
+    class Meta:
+        model = User
+        fields = tuple(User.REQUIRED_FIELDS) + (
+            User.USERNAME_FIELD,
+            User._meta.pk.name,
+            'password',
+            'email'
+        )
+
+    def create(self, validated_data):
+        if settings.get('SEND_ACTIVATION_EMAIL'):
+            with transaction.atomic():
+                user = User.objects.create_user(**validated_data)
+                user.is_active = False
+                user.save(update_fields=['is_active'])
+        else:
+            user = User.objects.create_user(**validated_data)
+        return user

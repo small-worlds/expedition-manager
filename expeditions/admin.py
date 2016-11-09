0877e6afe52e69a3a4c89a3e2a524104116c2ca1
Lane Farrow <laneatomic@gmail.com>
2016-11-09 11:28:10 -0600

    * remove v1 from url * add search and filtering to admin modules

--- expeditions/admin.py
+++ expeditions/admin.py
@@ -4,11 +4,14 @@ from expeditions.models import Expedition, Waypoint
 
 class ExpeditionAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'start_date', 'end_date', 'published')
+    search_fields = ('name', 'start_date')
+    list_filter = ('published', )
 
 
 class WaypointAdmin(admin.ModelAdmin):
     list_display = ('id', 'expedition', 'name', 'system', 'planet', 'datetime')
     list_filter = ('expedition', )
+    search_fields = ('name', 'expedition__name', 'system', 'planet', 'datetime')
 
 admin.site.register(Expedition, ExpeditionAdmin)
 admin.site.register(Waypoint, WaypointAdmin)
